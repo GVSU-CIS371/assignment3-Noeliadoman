1406519e9ada00ee40507eb2fce6b26512aca1d7
Noelia Doman <chavezn@mail.gvsu.edu>
2026-03-19 22:34:17 -0400

    Refactor Creamer.vue to use currentCreamer store

--- src/components/Creamer.vue
+++ src/components/Creamer.vue
@@ -1,47 +1,13 @@
 <template>
-  <!-- Only render if creamer is not "No Creamer" -->
-  <div v-if="creamer !== 'No Creamer'" class="froth" :style="frothStyle">
-    <div v-for="n in 5" :key="n" class="foam" :style="foamStyle"></div>
+  <div class="froth">
+    <div v-for=" in 5" class="foam"></div> 
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-
-const props = defineProps({
-  creamer: {
-    type: String,
-    required: true
-  }
-});
-
-// Adjust the froth color and height based on creamer type
-const frothStyle = computed(() => {
-  switch (props.creamer) {
-    case 'Milk':
-      return { backgroundColor: '#FFFDD0', height: '18%' };
-    case 'Cream':
-      return { backgroundColor: '#FFF8DC', height: '20%' };
-    case 'Half & Half':
-      return { backgroundColor: '#F5F5DC', height: '19%' };
-    default:
-      return {};
-  }
-});
-
-// Optional: foam color can match froth for consistency
-const foamStyle = computed(() => {
-  switch (props.creamer) {
-    case 'Milk':
-      return { backgroundColor: '#F5F5DC' };
-    case 'Cream':
-      return { backgroundColor: '#FAF0E6' };
-    case 'Half & Half':
-      return { backgroundColor: '#EEE8AA' };
-    default:
-      return {};
-  }
-});
+  import { computed } from "vue";
+  import { currentCreamer } from "../stores/beverage";
+  const bgColor = computed(() => currentCreamer.value.color);
 </script>
 
 <style lang="scss" scoped>
@@ -49,21 +15,48 @@ const foamStyle = computed(() => {
   overflow: visible;
   transform: translateY(400%);
   position: relative;
+  height: 20%;
   width: 100%;
+  background-color: #c6c6c6;
   animation: pour-tea 2s 2s forwards;
 }
-
 .foam {
   display: block;
-  border-radius: 50%;
+  background: v-bind(bgColor);
+  border-radius: 30px;
   height: 40px;
   width: 40px;
   position: absolute;
 }
 
-.foam:nth-child(1) { top: 0px; left: -3px; }
-.foam:nth-child(2) { top: 0px; left: 55px; }
-.foam:nth-child(3) { width: 30px; height: 30px; border-radius: 40px; top: 3px; left: 30px; }
-.foam:nth-child(4) { width: 30px; height: 30px; border-radius: 45px; top: 5px; right: -2px; }
-.foam:nth-child(5) { top: 2px; right: 10px; }
+.foam:nth-child(1) {
+  top: 0px;
+  left: -3px;
+}
+
+.foam:nth-child(2) {
+  top: 0px;
+  left: 55px;
+}
+
+.foam:nth-child(3) {
+  width: 30px;
+  height: 30px;
+  border-radius: 40px;
+  top: 3px;
+  left: 30px;
+}
+
+.foam:nth-child(4) {
+  width: 30px;
+  height: 30px;
+  border-radius: 45px;
+  top: 5px;
+  right: -2px;
+}
+
+.foam:nth-child(5) {
+  top: 2px;
+  right: 10px;
+}
 </style>
