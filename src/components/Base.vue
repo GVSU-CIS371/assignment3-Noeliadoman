17cfd994f8eb03810c696a2e12a11bb1524b8526
Noelia Doman <chavezn@mail.gvsu.edu>
2026-03-19 22:33:02 -0400

    recommended changes from classmate

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,48 +1,16 @@
 <template>
-  <div :style="baseStyle" class="baseBeverage">
-    {{ base }}
-  </div>
+  <div class="baseBeverage"></div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-
-const props = defineProps({
-  base: {
-    type: String,
-    required: true
-  }
-});
-
-// Dynamically set the background color based on the base beverage
-const baseStyle = computed(() => {
-  switch (props.base) {
-    case 'Coffee':
-      return {
-        backgroundColor: '#6F4E37',
-        height: '100%',
-        borderRadius: '0.05em 0.05em 2.2em 2.2em',
-      };
-    case 'Green Tea':
-      return {
-        backgroundColor: '#9ACD32',
-        height: '100%',
-        borderRadius: '0.05em 0.05em 2.2em 2.2em',
-      };
-    case 'Black Tea':
-      return {
-        backgroundColor: '#3B2F2F',
-        height: '100%',
-        borderRadius: '0.05em 0.05em 2.2em 2.2em',
-      };
-    default:
-      return {};
-  }
-});
+import { computed } from "vue";
+import { currentBase } from "../stores/beverage";
+const bgColor = computed(() => currentBase.value.color);
 </script>
 
 <style scoped>
 .baseBeverage {
+  background-color: v-bind(bgColor);
   position: relative;
   width: 100%;
   height: 100%;
@@ -51,3 +19,4 @@ const baseStyle = computed(() => {
   z-index: 300;
 }
 </style>
+
